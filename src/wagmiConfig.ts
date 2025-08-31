c897fad59a69e7cfd10f6547b7db10af414f3e97
OFUZOR CHUKWUEMEKE <uniben2018@gmail.com>
2025-08-31 10:33:30 +0100

    updated

--- src/wagmiConfig.ts
+++ src/wagmiConfig.ts
@@ -1,14 +1,16 @@
 import { baseSepolia, liskSepolia } from 'viem/chains';
 import { http, fallback } from 'wagmi';
 import { createConfig } from '@privy-io/wagmi';
+import { injected } from 'wagmi/connectors';
 
 
 export const wagmiConfig = createConfig({
-  chains: [baseSepolia, liskSepolia],
+  chains: [liskSepolia, baseSepolia],
+  connectors: [injected()],
   transports: {
+    [liskSepolia.id]: http(),
     [baseSepolia.id]: fallback([
       http('https://base-sepolia.gateway.tenderly.co'),
-    ]),
-    [liskSepolia.id]: http()
+    ])
   },
 });
