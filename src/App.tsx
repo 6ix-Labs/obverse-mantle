ff627289ce66307fa8f0e906680c81b359b604f2
Vinyl-Davyl <okononfuadavid@gmail.com>
2026-01-21 07:49:06 +0100

    update: development

--- src/App.tsx
+++ src/App.tsx
@@ -4,30 +4,27 @@ import Home from "./Pages/Home/Home";
 import About from "./Pages/About/About";
 import Error from "./Pages/Error/Error";
 import Footer from "./Components/Footer/Footer";
-import Wallet from "./Pages/Wallet/Wallet";
-import Payment from "./Pages/Payment/Payment";
 import { Toaster } from "./Components/ui/sonner";
-import Main from "./Pages/Waitlist/waitlist";
 import Payments from "./Pages/Payment/Payments";
 
 const AppRoutes = () => {
   const location = useLocation();
-  const hideNavbar = ["/transaction", "/pay", "/"];
+  const hideNavbar = ["/transaction", "/pay"];
   const shouldHide = hideNavbar.some((path) =>
     location.pathname.startsWith(path)
   );
 
   return (
     <main className="max-container">
-      {/* {!shouldHide && <Navbar />} */}
+      {!shouldHide && <Navbar />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="pay/:id" element={<Payments />} />
-        {/* <Route path="/" element={<Main />} />
         <Route path="about" element={<About />} />
+        <Route path="*" element={<Error />} /> 
+        {/* <Route path="/" element={<Main />} />
         <Route path="transactions/:linkId" element={<Wallet />} />
-        <Route path="pay/:id" element={<Payment />} />
-        <Route path="*" element={<Error />} /> */}
+        <Route path="pay/:id" element={<Payment />} /> */}
       </Routes>
       {/* Footer */}
       {!shouldHide && <Footer />}
