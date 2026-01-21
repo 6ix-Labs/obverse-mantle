50160f0411660ac3b40ae22e823c1e1a3c9854f4
Vinyl-Davyl <okononfuadavid@gmail.com>
2026-01-21 07:52:09 +0100

    update: development

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,41 +1,39 @@
-// import React from 'react'
-// import Hero from '../../sections/Hero/Hero'
-// import Herotext from '../../sections/Hero/Herotext'
-// import SendScanDone from '../../sections/SendScanDone/SendScanDone'
-// import BuiltFor from '../../sections/BuiltFor/BuiltFor'
-// import WhatMakes from '../../sections/WhatMakes/WhatMakes'
-// import PaymentApp from '../../sections/PaymentApp/PaymentApp'
-// import ReadyToAccept from '../../sections/ReadyToAccept/ReadyToAccept'
-// import Testimonials from '../../sections/Testimonials/Testimonials'
+import Hero from '../../sections/Hero/Hero'
+import Herotext from '../../sections/Hero/Herotext'
+import SendScanDone from '../../sections/SendScanDone/SendScanDone'
+import BuiltFor from '../../sections/BuiltFor/BuiltFor'
+import WhatMakes from '../../sections/WhatMakes/WhatMakes'
+import PaymentApp from '../../sections/PaymentApp/PaymentApp'
+import ReadyToAccept from '../../sections/ReadyToAccept/ReadyToAccept'
+import Testimonials from '../../sections/Testimonials/Testimonials'
+
+// import Built from "@/sections/newSections/Built"
+// import Faq from "@/sections/newSections/Faq"
+// import Footer from "@/sections/newSections/Footer"
+// // import Hero from "@/sections/newSections/Hero"
+// import Navbar from "@/sections/newSections/Navbar"
+// import { TimelineDemo } from "@/sections/newSections/TimeLineDemo"
 // import Main from '../Waitlist/waitlist'
-
-
-import Built from "@/sections/newSections/Built"
-import Faq from "@/sections/newSections/Faq"
-import Footer from "@/sections/newSections/Footer"
-import Hero from "@/sections/newSections/Hero"
-import Navbar from "@/sections/newSections/Navbar"
-import { TimelineDemo } from "@/sections/newSections/TimeLineDemo"
 
 const Home = () => {
   return (
-    <section className='flex flex-col space-y-14'>
-     <Navbar />
-     <Hero />
-     <Built />
-     {/* <TimelineDemo /> */}
-     <Faq />
-     <Footer />
-      {/* <Hero />
+    <section>
+      <Hero />
       <Herotext />
       <SendScanDone />
       <BuiltFor />
       <WhatMakes />
       <PaymentApp />
       <Testimonials />
-      <ReadyToAccept /> */}
+      <ReadyToAccept /> 
+      {/* <Navbar />
+      {/* <Hero /> */}
+      {/* <Built /> */}
+      {/* <TimelineDemo /> */}
+      {/* <Faq /> */}
+      {/* <Footer /> */}
+      {/* <Main/> */}
     </section>
-    // <Main/>
   )
 }
 
