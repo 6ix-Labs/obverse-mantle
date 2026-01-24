9a0f9dc10e7ae2f0a6edbbc90ea2597f26cd3ef9
Vinyl-Davyl <okononfuadavid@gmail.com>
2026-01-25 00:05:22 +0100

    dev: updated what makes obverse different

--- src/sections/WhatMakes/WhatMakes.tsx
+++ src/sections/WhatMakes/WhatMakes.tsx
@@ -1,11 +1,9 @@
-import React from 'react'
 import { WhatMakes1, WhatMakes2 } from '../../contants'
-import { isTemplateSpan } from 'typescript'
 import { motion } from "framer-motion"
 
 const WhatMakes = () => {
     return (
-        <section className='flex flex-col justify-center items-center gap-5 padding-t padding-x padding-y padding-b w-full max-container bg-background-main overflow-x-hidden'>
+        <section className='flex overflow-x-hidden flex-col gap-5 justify-center items-center w-full padding-t padding-x padding-y padding-b max-container bg-background-main'>
             <div className='flex items-center'>
                 <motion.h1
                     initial={{ opacity: 0 }}
@@ -15,7 +13,7 @@ const WhatMakes = () => {
                     className='sm:text-[55px] text-[36px] leading-text tracking-text font-calsans text-[#2e1109] text-center'>What Makes Obverse <br /> Different?</motion.h1>
             </div>
             <div className="grid gap-6 w-full lg:px-10">
-                <div className="grid md:grid-cols-2 grid-cols-1 gap-6 ">
+                <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
                     {
                         WhatMakes1.map((item) => (
                             <motion.div
@@ -23,11 +21,11 @@ const WhatMakes = () => {
                                 whileInView={{ opacity: 1, x: 0 }}
                                 transition={{ duration: 0.5, delay: 0.2 }}
                                 viewport={{ once: true }}
-                                key={item.label} className="bg-background-card rounded-[40px] flex flex-col lg:gap-5 p-4">
-                                <img src={item.icon} alt={item.label} className='flex h-[320px] w-[320px] items-center justify-center self-center object-contain object-center' />
-                                <div className='flex flex-col items-start gap-2'>
-                                    <h3 className='text-2xl font-semibold font-figtree flex text-brown'>{item.label}</h3>
-                                    <p className='text-[#000000] sm:text-[16px] text-[12px] md:max-w-lg font-figtree'>{item.text}</p>
+                                key={item.label} className="bg-background-card rounded-[40px] flex flex-col lg:gap-5 p-4 lg:p-8">
+                                <img src={item.icon} alt={item.label} className='flex h-[280px] w-[280px] lg:h-[320px] lg:w-[320px] items-center justify-center self-center object-contain object-center' />
+                                <div className='flex flex-col gap-2 items-start px-2 pb-2'>
+                                    <h3 className='flex text-2xl font-semibold font-figtree text-brown'>{item.label}</h3>
+                                    <p className='text-[#000000] sm:text-[16px] text-[14px] font-figtree leading-relaxed'>{item.text}</p>
                                 </div>
                             </motion.div>
 
@@ -36,7 +34,7 @@ const WhatMakes = () => {
 
 
                 {/* <!-- Bottom row with 3 equal items --> */}
-                <div className="grid xl:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6">
+                <div className="grid grid-cols-1 gap-6 xl:grid-cols-3 md:grid-cols-2">
                     {
                         WhatMakes2.map((item) => (
                             <motion.div
@@ -44,11 +42,11 @@ const WhatMakes = () => {
                                 whileInView={{ opacity: 1, x: 0 }}
                                 transition={{ duration: 0.5, delay: 0.2 }}
                                 viewport={{ once: true }}
-                                key={item.label} className="bg-background-card rounded-[40px] flex flex-col lg:gap-5 p-4">
-                                <img src={item.icon} alt={item.label} className='flex h-[320px] w-[320px] items-center justify-center self-center object-contain object-center' />
-                                <div className='flex flex-col items-start gap-2'>
-                                    <h3 className='text-2xl font-semibold font-figtree flex text-brown'>{item.label}</h3>
-                                    <p className='text-[#000000] sm:text-[16px] text-[12px] md:max-w-lg font-figtree'>{item.text}</p>
+                                key={item.label} className="bg-background-card rounded-[40px] flex flex-col lg:gap-5 p-6 lg:p-8">
+                                <img src={item.icon} alt={item.label} className='flex h-[280px] w-[280px] lg:h-[320px] lg:w-[320px] items-center justify-center self-center object-contain object-center' />
+                                <div className='flex flex-col gap-2 items-start'>
+                                    <h3 className='flex text-2xl font-semibold font-figtree text-brown'>{item.label}</h3>
+                                    <p className='text-[#000000] sm:text-[16px] text-[14px] font-figtree leading-relaxed'>{item.text}</p>
                                 </div>
                             </motion.div>
 
