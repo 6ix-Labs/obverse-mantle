75a6fe0cb22276d11bd1d783aeb469f5e57a6c6c
Vinyl-Davyl <okononfuadavid@gmail.com>
2026-01-25 13:28:17 +0100

    update: development

--- .next/static/chunks/pages/_error.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/[root-of-the-server]__bca07f49._.js",
-  "static/chunks/pages__error_2da965e7._.js",
-  "static/chunks/turbopack-pages__error_1f7fa559._.js"
-])
